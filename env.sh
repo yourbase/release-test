40406a4b6cf11602b66af16948dc8e426e3a2650
Herbert Duarte <herbertmd@users.noreply.github.com>
2019-11-07 11:59:11 +0000

    Add env.sh file to release-test

---
+++ env.sh
@@ -0,0 +1,11 @@
+set -xe
+echo "Sleeping for 10s"
+sleep 10
+echo "Git Commit: ${YB_GIT_COMMIT}"
+echo "Git Branch: ${YB_GIT_BRANCH}"
+echo "Samwise: ${SAMWISE}"
+
+env
+echo "OHAI..."
+echo "PEW PEW PEW.."
+echo "SNARFLE"
